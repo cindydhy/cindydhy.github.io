d356b4445a66b248f289d80f3150c36aa0d76f0f
Cindy Ding <cindydhy@hotmail.com>
2020-12-23 13:02:59 -0800

    added redirection

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
     <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      {/* <Route path='/cindydhy' component={() => { 
+    //  window.location.href = 'https://github.com/cindydhy'; 
+        window.open('https://github.com/cindydhy', '_blank');
+    //  return null;
+      }}/> */}
+      
     </Switch>
   </BrowserRouter>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,48 @@
 import React from "react";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import IconButton from '@material-ui/core/IconButton';
 import { withRouter } from "react-router-dom";
 
 class Home extends React.Component {
-    render() {
-        const useStyles = {
-            title: {
-                padding: "10px",
-                fontFamily: "Merriweather",
-                textAlign: "center",
-                textTransform: "uppercase",
-                color: "#484D51",
-                fontSize: "35px",
-                position: "absolute",
-                left: "38%",
-                top: "30%",
-            },
-            subheadings: {
-                position: "absolute",
-                left: "37%",
-                top: "43%",
-                color: "#484D51",
-                fontFamily: "B612",
-            },
-        };
-      return (
-        <div>
-            <h1 style={useStyles.title}>Hi, I'm Cindy Ding!</h1>
-            <p style={useStyles.subheadings}>Welcome to my website. This is a work in progress</p>
-        </div>
-      );
+
+  toGitHub = () => {
+    window.open('https://github.com/cindydhy', '_blank');
+  }
+
+  render() {
+    const useStyles = {
+        title: {
+            padding: "10px",
+            fontFamily: "Merriweather",
+            textAlign: "center",
+            textTransform: "uppercase",
+            color: "#484D51",
+            fontSize: "35px",
+            position: "absolute",
+            left: "38%",
+            top: "30%",
+        },
+        icons: {
+          position: "absolute",
+          left: "45%",
+          top: "43%",
+      },
+      subheadings: {
+        position: "absolute",
+        left: "37%",
+        top: "43%",
+        color: "#484D51",
+        fontFamily: "B612",
+    },
+  };
+    return (
+      <div>
+          <h1 style={useStyles.title}>Hi, I'm Cindy Ding!</h1>
+          <IconButton style={useStyles.icons} onClick={this.toGitHub}>
+            <GitHubIcon />
+          </IconButton>
+      </div>
+    );
     }
   }
 
